13cd93b94241b3a1df01f135dfe37f4c73dcc35d
Sergio Orozco <scot3004@gmail.com>
2023-09-06 21:11:07 +0000

    Demo S3 Transform to csv

--- src/taxis_etl_prft/DemoS3.py
+++ src/taxis_etl_prft/DemoS3.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
+from pyspark.sql.functions import col, unix_timestamp
+
+from functools import reduce
 
 
 load_dotenv()
@@ -13,8 +16,14 @@ spark = SparkSession.builder.appName("DemoS3").config(conf=conf).getOrCreate()
 bucket = os.getenv("AWS_S3_BUCKET")
 print("Bucket {}".format(bucket))
 filenames = os.getenv("TAXIS_ETL_FILES").split(",")
-for key in filenames:
-    print("s3a://{}/{}".format(bucket, key))
-    df = spark.read.parquet("s3a://{}/{}".format(bucket, key))
 
-spark.stop() 
+parquets = map(lambda key: spark.read.parquet("s3a://{}/{}".format(bucket, key)), filenames)
+joined_parquets = reduce(lambda x, y: x.union(y), parquets)
+
+trip_distance_km = joined_parquets.withColumn("trip_Distance_kms", col("trip_distance") * 1.6)
+final_transform = trip_distance_km.withColumn("travel_time",
+      (unix_timestamp(col("tpep_dropoff_datetime")) - unix_timestamp(col("tpep_pickup_datetime"))) / 60)
+
+final_transform.write.option("delimiter", "\t").option("header", "true").mode("overwrite").csv("results/csv")
+
+spark.stop()
